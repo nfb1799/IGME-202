20a3603c91ca77ef6570d374c4e6c116a83988e0
Nik Barbero (RIT Student) <nfb1799@ad.rit.edu>
2020-01-28 15:17:43 -0500

    Random Walker Exercise
    Base system for random walker movement implemented

---
+++ Barbero_Random/Assets/Scripts/RandomWalk.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWalk : MonoBehaviour
+{
+    public bool walking;
+    // Start is called before the first frame update
+    void Start()
+    {
+        walking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w") && Input.GetKey("d"))
+            transform.Translate(1, 0, 1);
+        else if (Input.GetKey("a") && Input.GetKey("w"))
+            transform.Translate(-1, 0, 1);
+        else if (Input.GetKey("a") && Input.GetKey("s"))
+            transform.Translate(-1, 0, -1);
+        else if (Input.GetKey("d") && Input.GetKey("s"))
+            transform.Translate(1, 0, -1);
+        else if (Input.GetKey("a"))
+            transform.Translate(-1, 0, 0);
+        else if (Input.GetKey("d"))
+            transform.Translate(1, 0, 0);
+        else if (Input.GetKey("w"))
+            transform.Translate(0, 0, 1);
+        else if (Input.GetKey("s"))
+            transform.Translate(0, 0, -1);
+
+        if (Input.GetKeyDown("m"))
+            walking = !walking;
+
+        if (walking)
+            transform.Translate(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
+
+
+    }
+}
---
+++ Rotation_Exercise/Assets/Scripts/TerrainComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainComponent : MonoBehaviour
+{
+    private TerrainData myTerrainData; 
+    public Vector3 worldSize; 
+    public int resolution = 129; 
+    float[,] heightArray;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
