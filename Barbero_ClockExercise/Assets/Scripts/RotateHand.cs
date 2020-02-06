e080ebb23578d21bd38b756c7d36c5696b75e004
Nik Barbero (RIT Student) <nfb1799@ad.rit.edu>
2020-02-06 15:24:04 -0500

    Clock Exercise
    Adjusting functionality of the "Button" script

---
+++ Barbero_ClockExercise/Assets/Scripts/RotateHand.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateHand : MonoBehaviour
+{
+    Vector3 mouseWorldPos;
+    float theta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            RotateToMouse();
+    }
+
+    void RotateToMouse()
+    {
+        mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        theta = 180.0f + (Mathf.Atan2(mouseWorldPos.y, mouseWorldPos.x) * Mathf.Rad2Deg);
+        transform.rotation = Quaternion.Euler(0, 0, theta);
+    }
+}
