6a827f9fa156f8c210419fbe29c6b35d64650933
nfb1799 <54686118+nfb1799@users.noreply.github.com>
2020-02-18 11:13:31 -0500

    clock

--- Barbero_ClockExercise/Assets/Scripts/ClockNumbers.cs
+++ Barbero_ClockExercise/Assets/Scripts/ClockNumbers.cs
@@ -5,10 +5,33 @@ using UnityEngine;
 public class ClockNumbers : MonoBehaviour
 {
     public GameObject prefab;
+    private const float radius = 2.0F;
+    private float deg;
+    private float rad;
+    private float x;
+    private float y;
     // Start is called before the first frame update
     void Start()
     {
-        prefab.GetComponentInChildren<TextMesh>().text = "12";
+        deg = 0.0F;
+  
+        for (int i = 1; i <= 12; i++)
+        {
+            prefab.GetComponentInChildren<TextMesh>().text = i.ToString();
+            rad = Mathf.Deg2Rad * deg;
+            x = radius * Mathf.Acos(rad);
+            y = radius * Mathf.Asin(rad);
+            Instantiate(prefab, new Vector3(x, y, 0), Quaternion.identity);
+            deg += 30.0F;
+        }
+        /*for (int i = 1; i <= 12; i++)
+        {
+            prefab.GetComponentInChildren<TextMesh>().text = i.ToString();
+            x = radius * Mathf.Acos(Mathf.Deg2Rad * (30.0F * i));
+            y = radius * Mathf.Asin(Mathf.Deg2Rad * (30.0F * i));
+            Instantiate(prefab, new Vector3(x, y, 0), Quaternion.identity);
+        }
+        /*prefab.GetComponentInChildren<TextMesh>().text = "12";
         Instantiate(prefab, new Vector3(0, 2.3F, 0), Quaternion.identity);
         prefab.GetComponentInChildren<TextMesh>().text = "1";
         Instantiate(prefab, new Vector3(1.25F, 2.0F, 0), Quaternion.identity);
@@ -32,7 +55,7 @@ public class ClockNumbers : MonoBehaviour
         Instantiate(prefab, new Vector3(-2.0F, 1.2F, 0), Quaternion.identity);
         prefab.GetComponentInChildren<TextMesh>().text = "11";
         Instantiate(prefab, new Vector3(-1.25F, 2.0F, 0), Quaternion.identity);
-
+        */
     }
 
     // Update is called once per frame
