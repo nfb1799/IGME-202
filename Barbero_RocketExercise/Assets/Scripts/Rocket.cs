58768f75a5e4db266fc282ab9dacc7d1fb788e8a
nfb1799 <54686118+nfb1799@users.noreply.github.com>
2020-02-18 12:35:59 -0500

    Rocket Draft
    Exercise

--- Barbero_RocketExercise/Assets/Scripts/Rocket.cs
+++ Barbero_RocketExercise/Assets/Scripts/Rocket.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    float v = 100f;
+    float v = 50f;
     float theta = Mathf.Deg2Rad * 60f;
-    float h = 0.22f;
+    float h = 4.5f;
     float g = 32f;
-    float x, y, z;
+    float thrust = 64f;
+    float x, y, z, force;
     float tT = 0f;
-    float stT = 1.0f;
+    float stT = 0.1f;
 
     Vector3 acc;
     Vector3 vel;
@@ -24,7 +25,7 @@ public class Rocket : MonoBehaviour
         acc = new Vector3(0f, -g, 0f); //constant
         vel = new Vector3(v * Mathf.Cos(theta), v * Mathf.Sin(theta), 0f);  //initial velocity
 
-        x = -8.5f;  //initial positions
+        x = 0f;  //initial positions
         y = h;
         z = 0f;
 
@@ -36,23 +37,30 @@ public class Rocket : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
             launched = !launched;
+            force = g - thrust;
+        }
 
         if (launched)
         {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+                force = g;
+
             tT += Time.deltaTime;
             x = v * Mathf.Cos(theta) * (stT * tT);
-            y = h + v * Mathf.Sin(theta) * (stT * tT) - 0.5f * g * (stT * tT) * (stT * tT);
+            y = h + v * Mathf.Sin(theta) * (stT * tT) - 0.5f * force * (stT * tT) * (stT * tT);
             pos = new Vector3(x, y, z);
         }
         else
         {
             tT = 0.0f;
-            x = -8.5f;  //initial positions
+            x = 0f;  //initial positions
             y = h;
             z = 0f;
             pos = new Vector3(x, y, z);
         }
+        
 
         transform.position = pos;
     }
